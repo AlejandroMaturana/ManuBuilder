aa735e663940b27f3f1e912921081ad22d7ae9f9
ManuGL <neofito331@gmail.com>
2026-06-20 21:20:26 -0400

    feat: registrar rutas de configuración y cotizaciones

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,9 @@ import Clientes from './pages/Clientes';
 import Proyectos from './pages/Proyectos';
 import ProyectoDetalle from './pages/ProyectoDetalle';
 import Stock from './pages/Stock';
+import Configuracion from './pages/Configuracion';
+import Cotizaciones from './pages/Cotizaciones';
+import CotizacionForm from './pages/CotizacionForm';
 
 export default function App() {
   return (
@@ -16,6 +19,10 @@ export default function App() {
           <Route path="/proyectos/:id"      element={<ProyectoDetalle />} />
           <Route path="/clientes"           element={<Clientes />} />
           <Route path="/stock"              element={<Stock />} />
+          <Route path="/configuracion"      element={<Configuracion />} />
+          <Route path="/cotizaciones"       element={<Cotizaciones />} />
+          <Route path="/cotizaciones/nueva" element={<CotizacionForm />} />
+          <Route path="/cotizaciones/:id"   element={<CotizacionForm />} />
         </Routes>
       </Layout>
     </BrowserRouter>
